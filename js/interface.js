b021cb5c8b6d5a8d28c61c35cc08385d7684120f
David Balboa <david@devils.local>
2019-10-17 17:56:49 +0200

    actualizacion

--- js/interface.js
+++ js/interface.js
@@ -1,7 +1,15 @@
 /* Invocar la ventana emergente por defecto */
-window.open("./html/countdays.html", " ", "width=400,height=400");
+document.addEventListener('pageshow', function () {
+    /* Invocar la ventana emergente al pulsar el boton */
+    window.open("./html/countdays.html", "calc", "width=400,height=400");
+});
 
 document.getElementById("home").addEventListener('click', function () {
     /* Invocar la ventana emergente al pulsar el boton */
-    window.open("./html/countdays.html", " ", "width=400,height=400");
+    window.open("./html/countdays.html", "calc", "width=400,height=400");
+});
+
+document.getElementById("botClose").addEventListener('click', function () {
+    /* Invocar la ventana emergente al pulsar el boton */
+    calc.close();
 });
