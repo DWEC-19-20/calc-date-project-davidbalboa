68559a010db69a3f22fb0209039c86e481303425
David Balboa <david@devils.local>
2019-10-17 17:58:30 +0200

    actualizacion interface

--- js/interface.js
+++ js/interface.js
@@ -1,15 +1,7 @@
 /* Invocar la ventana emergente por defecto */
-document.addEventListener('pageshow', function () {
-    /* Invocar la ventana emergente al pulsar el boton */
     window.open("./html/countdays.html", "calc", "width=400,height=400");
-});
 
 document.getElementById("home").addEventListener('click', function () {
     /* Invocar la ventana emergente al pulsar el boton */
     window.open("./html/countdays.html", "calc", "width=400,height=400");
-});
-
-document.getElementById("botClose").addEventListener('click', function () {
-    /* Invocar la ventana emergente al pulsar el boton */
-    calc.close();
 });
